6b793e56ec646494449b724230ecb5f298191eec
Maurizio Napolitano <napo@fbk.eu>
2020-03-20 11:22:53 +0100

    update

--- assets/js/apss/map.js
+++ assets/js/apss/map.js
@@ -3,31 +3,40 @@ require(['jquery', 'vector-map', 'vector-map-trentino'], function(){
         var markers = []
         var dataformap = {};
         codicicomuni = {};
+        abitanticomuni = {};
+        function each1000people(idc,m) {
+            c = 1000;
+            p = abitanticomuni[idc] 
+            n =(m*c)/p
+            return(n)
+        }
+
         $.each(tablecodicicomuni, function( index, row ) {
             if (index !=0) {
                 codicicomuni[row[1].toUpperCase()] = row[0];
+                abitanticomuni[row[0]]=parseInt(row[2]);
                 dataformap[row[0]] = 0;
             }
         });
-        var addcarano = 0;
+
         $.each(tablesituazionecomuni, function( index, row ) {
             if (index !=0) {
                 nomecomune = row[0];
                 v = parseInt(row[1]);
-                //work around causa errore
-                if (nomecomune!='CARANO') {
-                    dataformap[codicicomuni[nomecomune]]=v;
+                if (codicicomuni.hasOwnProperty(nomecomune)) {
+                    v1 = each1000people(codicicomuni[nomecomune],v);
+                    v1= Math.round(v1 * 10) / 10;
+                    dataformap[codicicomuni[nomecomune]]=v1;
+                    console.log(v1);
                     m = {latLng: [row[2], row[3]], name: nomecomune};
                     markers.push(m);
-                } else {
-                    addcarano = parseInt(row[1]);
-                }
+                } 
             }
         });
         //work around perche' carano si e' fuso con Ville di Fiemem
 
-        dataformap["254"] = dataformap["254"] + addcarano;
-
+        //dataformap["254"] = dataformap["254"] + addcarano;
+        //console.log(dataformap);
         $('#map-trentino-svg').vectorMap({
             map: 'comuni_trentini',
             legend: {
